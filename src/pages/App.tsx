43fd2df3d21970bae9d7dd831f6326ddd1f8d027
JPNAKAYAMA <jpn_rufino@hotmail.com>
2022-07-01 15:06:07 -0300

    alura-studies - v1.0

---
+++ src/pages/App.tsx
@@ -0,0 +1,55 @@
+import { render } from '@testing-library/react';
+import Button from '../components/button';
+import Formulario from '../components/formulario';
+import Lista from '../components/lista';
+import style from './App.module.scss';
+import Cronometro from '../components/cronometro';
+import React, { useState } from 'react';
+import ITarefa from '../types/tarefa';
+
+function App() {
+  const [tarefas, setTarefas] = useState< Array<ITarefa>>([])
+  const [selecionado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa) 
+  {
+    setSelecionado(tarefaSelecionada);
+    setTarefas(tarefasAnteriores => tarefasAnteriores.
+      map(tarefa => ({
+      ... tarefa,
+      selecionado: tarefa.id === tarefaSelecionada.id ? true : false 
+    })));
+  }
+
+  function finalizarTarefa() {
+    if(selecionado) {
+      setSelecionado(undefined);
+      setTarefas(tarefasAnteriores => tarefasAnteriores.map (tarefa => {
+        if(tarefa.id === selecionado.id) {
+          return {
+            ... tarefa,
+            selecionado: false,
+            completado: true
+          }
+        }
+        return tarefa;
+      }))
+    }
+  }
+
+  return (
+    <div className={style.AppStyle}>
+       <Formulario setTarefas={setTarefas} />
+       <Lista
+        tarefas = {tarefas}
+        selecionaTarefa = {selecionaTarefa}
+      />
+       <Cronometro 
+       selecionado={selecionado}
+       finalizarTarefa = {finalizarTarefa}
+       />
+    </div>
+  );
+}
+
+export default App;
